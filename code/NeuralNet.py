b9ad92a3f2207b749a88ba7e4b8e6f9fe52970af
Jake Kuo <96745417+jakekuo888@users.noreply.github.com>
2026-06-17 21:34:18 +0000

    Simple NN class

--- code/NeuralNet.py
+++ code/NeuralNet.py
@@ -1,14 +1,26 @@
 import torch
+import numpy as np
 import pandas as pd
+
 from torch import nn
 
 class NeuralNet(nn.Module):
-  def __init__(self):
+  def __init__(self, in_size, out_size, hidden_sizes=[64, 64]):
     super().__init__()
-    self.flatten = nn.Flatten()
-    #insert any other important things
+    
+    self.inS = in_size
+    self.outS = out_size
+    self.hidS = hidden_sizes
+    self.layers = []
+    p_size = self.inS
+
+    #set layers
+    for h in self.hidS:
+      self.layers.append(nn.Linear(p_size, h))
+      self.layers.append(nn.ReLU())
+      p_size = h
+    self.layers.append(nn.Linear(p_size, self.outS))
+    self.net = nn.Sequential(*self.layers)
 
   def forward(self, inp):
-    x = self.flatten(x)
-
-#more stuff
+    return self.net(inp)
