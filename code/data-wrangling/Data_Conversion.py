3423d0ef1c9825795cb2f2ae9e090f53c688330d
Jake Kuo <96745417+jakekuo888@users.noreply.github.com>
2026-06-18 00:15:25 +0000

    Data conversion prelim; edit later

--- code/data-wrangling/Data_Conversion.py
+++ code/data-wrangling/Data_Conversion.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import Model_Generator
+from Model_Generator import smiles_to_graph
+
 
 # DATA: https://www.nature.com/articles/s41597-020-00634-8
 
@@ -10,5 +12,13 @@ column_headers = chromophore_df.columns.to_list()
 chromophore_df = chromophore_df.drop(columns=[header for header in column_headers if header not in ("Chromophore", "Solvent", "Lifetime (ns)")])
 
 chromophore_df.to_csv(dest_path, index = False)
+
+#molecule
 for smiles in chromophore_df.iloc[:, 0]:
-    print(smiles)
+    nData = smiles_to_graph(smiles)
+
+#solvent
+for smiles in chromophore_df.iloc[:, 1]:
+    nData = smiles_to_graph(smiles)
+
+#print(smiles_to_graph(chromophore_df.iloc[1, 0]))
