f085ab4e96134f3db7ff862be4162a1c1af530a4
FallingJungle41 <krish.nandola07@gmail.com>
2026-06-23 12:32:53 -0700

    combat overfitting w/ weight decay & skip-connections

--- code/neural_networks.py
+++ code/neural_networks.py
@@ -80,8 +80,10 @@ class GNN(nn.Module):
     x = self.conv1(x, edge_index, edge_attr)
     x = torch.relu(x)
 
+    identity = x
     x = self.conv2(x, edge_index, edge_attr)
     x = torch.relu(x)
+    x += identity
 
     x = self.conv3(x, edge_index, edge_attr)
     x = torch.relu(x)
