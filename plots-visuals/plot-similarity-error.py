e23d144a1f72602595c90e38004b656f6959bc74
Quasistar <96745417+Quasistarsupernova@users.noreply.github.com>
2026-06-26 15:45:42 -0700

    delete this

--- plots-visuals/plot-similarity-error.py
+++ plots-visuals/plot-similarity-error.py
@@ -49,3 +49,5 @@ def plot_vector_similarity_loss_graph(train_vec, test_vec, loss_list):
 			if temp_sim > max_dice_sim:
 				max_dice_sim = temp_sim
 		output_similarities.append(max_dice_sim)
+
+	plt.scatter(output_similarities, loss_list, 'ro', markersize = 10)
