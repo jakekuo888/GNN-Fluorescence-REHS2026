7d6f7a5f7906675e2b196b2caa521649b8a0e914
Quasistar <96745417+Quasistarsupernova@users.noreply.github.com>
2026-06-26 13:55:09 -0700

    Changed the plot-similarity-error.py to take in the data from txt.

--- plots-visuals/plot-similarity-error.py
+++ plots-visuals/plot-similarity-error.py
@@ -5,9 +5,24 @@ from rdkit import DataStructs
 import os
 import matplotlib.pyplot as plt
 
-"""
+#title and other information on graph
+plt.title(f'Error vs Similarity of molecules')
 
-"""
+#data parsing & finding similarity (incomplete)
+actual = []
+predicted = []
+mol_smiles = []
+solvents = []
 
-with open("")
-mol_solv = []
+#Reading in data
+with open("./data/plot-data/plot-similarity.txt") as f:
+	for line in f.read().splitlines():
+		items = line.split(',')
+		actual.append(float(items[0]))
+		predicted.append(float(items[1]))
+		mol_smiles.append(items[2])
+		solvents.append(items[3])
+
+mol = []
+for m in mol_smiles:
+	mol.append(Chem.MolFromSmiles(m))
