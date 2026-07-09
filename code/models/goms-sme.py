967f933dfccb6471e367da49fb3fa2a087681c66
Quasistar <96745417+Quasistarsupernova@users.noreply.github.com>
2026-07-09 12:51:17 -0700

    gmos-sme.py 1.0.1

--- code/models/goms-sme.py
+++ code/models/goms-sme.py
@@ -5,28 +5,35 @@ from egnn_pytorch import EGNN as EGNNLayer
 from torch_geometric.utils import to_dense_batch
 
 class FragEGNN(nn.Module):
-	def __init__(self, feat_dim, num_layers = 3):
+	def __init__(self, node_features, edge_features, num_layers = 3):
 		super().__init__()
 		
-		self.feat_dim = feat_dim
+		self.node_features = node_features
+		self.edge_features = edge_features
+		#Note these are the dimensions and not the features themselves
+
 		self.num_layers = num_layers
 
-		#change this is we want the model to have a different architecture
-		#right now it's just num_layers of layers of size feat_dim
 		self.layers = nn.ModuleList([
-			EGNNLayer (dim = feat_dim) for _ in range(num_layers)
+			EGNNLayer (dim = node_features, edge_dim=edge_features) for _ in range(num_layers)
 		])
 
-	def forward(self, x, pos, batch):
+	def forward(self, x, pos, edge_index, edge_attr, batch):
 		#needs some preprocessing to seperate pos from x?
 
-		feats, mask = to_dense_batch(x, batch)
-		coors, _ = to_dense_batch(pos, batch)
+        feats, mask = to_dense_batch(x, batch)
+        coors, _ = to_dense_batch(pos, batch)
 
-		for layer in self.layers:
-			feats, coors = layer(feats, coors, mask=mask) #msg passing
+        edges = to_dense_adj(
+            edge_index,
+            batch=batch,
+            edge_attr=edge_attr
+        )
 
-		mask_f = mask.unsqueeze(-1).float()
-		frag_vectors = (feats * mask_f).sum(dim = 1) / mask_f.sum(dim = 1).clamp(min=1)
+        for layer in self.layers:
+            feats, coors = layer(feats, coors, edges, mask=mask)
 
-		return frag_vectors
+        mask_f = mask.unsqueeze(-1).float()
+        frag_vectors = (feats * mask_f).sum(dim=1) / mask_f.sum(dim=1).clamp(min=1)
+
+        return frag_vectors
