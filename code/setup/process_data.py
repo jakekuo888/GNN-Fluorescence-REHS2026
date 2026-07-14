ff972fa02caf92dcffc518940d1424a832554802
Quasistar <96745417+Quasistarsupernova@users.noreply.github.com>
2026-07-14 12:24:14 -0700

    small little change

--- code/setup/process_data.py
+++ code/setup/process_data.py
@@ -128,37 +128,6 @@ def collate_fn(mol_dicts):
     atom_batch = Batch.from_data_list(all_fragments)
     return atom_batch, frags_per_mol, mol_dicts
 
-
-def generate_GOMS(dicts, frags):
-    GOMS = []
-
-    for d, fs in zip(dicts, frags):
-        n_frags = len(fs)
-        x_ = torch.tensor(fs, dtype = torch.float)
-
-        cut_bonds = []
-
-        e_idx = 
-
-        #each d is dict for mol x
-        #fs is frags for mol x
-
-        for f in fs:
-            f
-        
-        graph = Data(
-            x = x_,
-            edge_index = e_idx,
-            edge_attr = edge_attr,
-            pos = pos,
-            y = y,
-        )
-
-        GOMS.append(graph)
-
-    return GOMS
-
-
 if re_generate_data:
     for option in absorption_data_options:
         generate_graphs_labels(option, generate_data=True)
